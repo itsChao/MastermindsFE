e1aa6590816c2e2605b0c3e079f3500c55650cc8
Fl0ri4nSchw <florian.schwarzentruber@hotmail.com>
2022-08-09 16:23:56 +0200

    Erray und Scanner

--- MastermindsFE/src/Main.java
+++ MastermindsFE/src/Main.java
@@ -1,2 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        String[] colors={"yellow", "red", "white", "blue", "pink", "orange"};
+        Random r=new Random();
+        int randomColor1=r.nextInt(colors.length);
+        int randomColor2=r.nextInt(colors.length);
+        int randomColor3=r.nextInt(colors.length);
+        int randomColor4=r.nextInt(colors.length);
+
+        System.out.println(colors[randomColor1]);
+        System.out.print(colors[randomColor2]);
+        System.out.print(colors[randomColor3]);
+        System.out.print(colors[randomColor4]);
+
+
+
+
+        Scanner color = new Scanner(System.in);
+        String first = color.nextLine();
+        String second = color.nextLine();
+        String third = color.nextLine();
+        String fourth = color.nextLine();
+
+        System.out.printf(first + " " + second + " " + third + " " + fourth);
+    }
 }
